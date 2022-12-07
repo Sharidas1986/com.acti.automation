4acbf50c664b2425423afe8e4e8c84c3a5f15ca5
Shijudas <sharidas@prontoforms.com>
2022-12-07 16:52:50 -0500

    Moved vommon code to BaseTestClass

--- src/test/java/com/acti/testcase/LoginPageTest.java
+++ src/test/java/com/acti/testcase/LoginPageTest.java
@@ -3,17 +3,13 @@ package com.acti.testcase;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import come.acti.base.DriverManager;
-import come.acti.page.HomePage;
-import come.acti.page.LoginPage;
+public class LoginPageTest extends BaseTest {
 
-public class LoginPageTest extends DriverManager {
 	
 
 	@Test
 	public void testLoginPageTitle() {
-		initApplication();
-		LoginPage lp = new LoginPage();
+
 		String actual = lp.getActiTitle();
 		String expected = "actiTIME - Login";
 		Assert.assertEquals(actual, expected);
@@ -22,39 +18,33 @@ public class LoginPageTest extends DriverManager {
 
 	@Test
 	public void testLoginPageImg() {
-		initApplication();
-		LoginPage lp = new LoginPage();
+
 		boolean flag = lp.actiImageLogo();
 		Assert.assertTrue(flag);
 		quitDriver();
 
 	}
-	
-	
+
 	@Test
-	public void testForgotPasswordLink()
-	{
-		initApplication();
-		LoginPage lp = new LoginPage();
+	public void testForgotPasswordLink() {
+
 		boolean fplink = lp.ForgotPassword();
 		Assert.assertTrue(fplink);
-		lp.quitDriver();
-		
+		quitDriver();
+
 	}
-	
+
 	@Test
-	public void testLoginFunction()
-	{
-		initApplication();
-		LoginPage lp = new LoginPage();
+	public void testLoginFunction() {
+
 		lp.username("admin");
 		lp.password("manager");
 		lp.clickLogin();
-		HomePage hp = new HomePage();
+
 		String actualhpTitle = hp.getHomePageTitle();
 		String expectedTtile = "actiTIME - Enter Time-Track";
 		Assert.assertEquals(actualhpTitle, expectedTtile);
 		quitDriver();
-		
+
 	}
 }
